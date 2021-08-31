3a6298f5550065ff8850774dd358f3c81796208f
Caritor <pallab@caritorsolutions.in>
2021-08-31 22:47:40 +0530

    q

--- akai/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import pallabdef
-
-urlpatterns = [
-    path('pallab/', pallabdef),
-]
--- akai/views.py
+++
@@ -1,8 +0,0 @@
-from django.http.response import HttpResponse
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def pallabdef(*args, **kwargs):
-    print("akai")
-    return HttpResponse("<h1>Akai is a bad boy<h1>")
