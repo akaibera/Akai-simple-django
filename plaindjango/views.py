8bb210d0876123ad168ea3f8c6510602d3384924
Caritor <pallab@caritorsolutions.in>
2021-08-31 23:17:51 +0530

    print removed

--- plaindjango/views.py
+++ plaindjango/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def pallabdef(*args, **kwargs):
-    print("akai")
-    return HttpResponse("<h1>Akai is a bad boy<h1>")
+    #print("akai")
+    #return HttpResponse("<h1>Akai is a bad boy<h1>")
+    return HttpResponse("<h1>pallab is a bad boy<h1>")
