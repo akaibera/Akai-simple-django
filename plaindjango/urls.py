8180b43d188ef492e4978c11f8136403079e3c5e
Caritor <pallab@caritorsolutions.in>
2021-08-31 22:34:23 +0530

    new app added

--- plaindjango/urls.py
+++ plaindjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import pallabdef
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('akai/', pallabdef),
 ]
